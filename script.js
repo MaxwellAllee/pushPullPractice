4e7c550a3530a56c2ff9091e99e43c3c3c146001
Jeffery Hoffman <jhoffman1@2u.com>
2020-08-13 14:50:37 -0400

    Merged

--- script.js
+++ script.js
@@ -4,11 +4,5 @@ var anotherOne = 3;
 
 if (newVariable) console.log("test");
 
-<<<<<<< HEAD
-
-
-var createdThirdvar = true
-var fourthVar=false
-=======
 var createdThirdvar = true;
->>>>>>> 0f05288d5b6653701e5ddcef7703b47d2f479496
+var fourthVar = false;
